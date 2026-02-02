561b77cd402d6014853cf2de38641464af310900
Ashlok Kumar <ashlokkumar2006@gmail.com>
2026-02-02 22:21:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
     en: {
         // header
       headerTitle: "ATM Simulator System Project",
-      headerDesc: "ATM Simulator System Project is a mini web application developed using
+      headerDesc: `ATM Simulator System Project is a mini web application developed using
       HTML, CSS and JavaScript. This ATM Simulator System project simulates
       real banking operations such as cash withdrawal, deposit, balance enquiry,
-      mini statement and PIN change.",
+      mini statement and PIN change.`,
       // nav
       featureWithdraw: "Cash Withdrawal",
       featureWithdrawDesc: "– Withdraw available cash from account",
@@ -1144,5 +1144,6 @@ const hoverMap = {
   
 
 }); // DOMContentLoaded end
+
 
 
